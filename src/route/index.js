e07285d7e5be9f9b9981ad9943eb2ce0d88ee7e5
Stloz <stloz.serg@gmail.com>
2023-09-08 21:19:33 +0300

    practice part 1

--- src/route/index.js
+++ src/route/index.js
@@ -5,21 +5,92 @@ const router = express.Router()
 
 // ================================================================
 
+//todo ============================================================
+
+class User {
+  static #list = []
+
+  constructor(email, login, password) {
+    this.email = email
+    this.login = login
+    this.password = password
+    this.id = new Date().getTime()
+  }
+
+  static add = (user) => {
+    this.#list.push(user)
+  }
+
+  static getList = () => {
+    return this.#list
+  }
+
+  static getById = (id) =>
+    this.#list.findIndex((user) => user.id === id)
+}
+
+//todo ============================================================
+
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
+  const list = User.getList()
+
   // ↙️ cюди вводимо назву файлу з сontainer
   res.render('index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
     style: 'index',
+
+    data: {
+      users: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
   })
   // ↑↑ сюди вводимо JSON дані
 })
 
+//todo ================================================================
+
+router.post('/user-create', function (req, res) {
+  const { email, login, password } = req.body
+
+  const user = new User(email, login, password)
+
+  User.add(user)
+
+  console.log(User.getList())
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: 'Користувач створний',
+  })
+})
+
 // ================================================================
+
+router.get('/user-delete', function (req, res) {
+  const { id } = req.query
+
+  console.log(typeof id)
+
+  const user = User.getById(Number(id))
+
+  if (user) {
+    console.log('!!!!!!!!!!!!!')
+  }
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: 'Користувач видалений',
+  })
+})
+
+//todo ================================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
